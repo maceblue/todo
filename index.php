b059286a49ce3c324d94ad51a1c72dd1fca366c7
m.mende <m.mende@axilaris.de>
2017-11-23 17:15:18 +0100

    speicherung umbau

--- index.php
+++ index.php
@@ -28,63 +28,98 @@
 
 <script language="javascript">
 var i = localStorage.length;
+var list_id = list_1;
+
 function onBodyLoad(){
 	var todo = "";
 	
-	create_new_list();
+	// create_new_list();
+	get_list();
 	
-	$("#clear").click(function(){
-		localStorage.clear();
-		$("#todo_list li").fadeOut(function(){
-			$(this).html("");
-		});
-	});
+	// $("#clear").click(function(){
+	// 	localStorage.clear();
+	// 	$("#todo_list li").fadeOut(function(){
+	// 		$(this).html("");
+	// 	});
+	// });
 
 	$("#remove").live("click",function(e){
 		var index = $(this).closest("li").attr("id");
 		$(this).closest("li").slideUp(function(){
 		
 			// remove the selected item
-			localStorage.removeItem('names_'+index);
+			//localStorage.removeItem('names_'+index);
 			
 			// rearrange localstorage array 
-			for(i=0; i<localStorage.length; i++) {
-			  if( !localStorage.getItem("names_"+i)) {
-				localStorage.setItem("names_"+i, localStorage.getItem('names_' + (i+1) ) );
-				localStorage.removeItem('names_'+ (i+1) );
-			  }
-			}
+			// for(i=0; i<localStorage.length; i++) {
+			//   if( !localStorage.getItem("names_"+i)) {
+			// 	localStorage.setItem("names_"+i, localStorage.getItem('names_' + (i+1) ) );
+			// 	localStorage.removeItem('names_'+ (i+1) );
+			//   }
+			// }
+
 			
 			// clear existing list UI
 			$("#todo_list").html("");
+
+			$.ajax({
+	            type: 'POST',
+	            url: 'ajax.php?do=delete_entry&list_id=' + list_id + '&entry=' + index,
+	            contentType: "application/json; charset=utf-8",
+	            data: '',
+	            timeout: 500,
+	            success: 'get_list'
+	        })
 			
-			// create new list
-			create_new_list();
 		});
 	});
 }
-function create_new_list(){
-	for (var i = 0; i < localStorage.length; i++){
-		todo = localStorage.getItem('names_'+i);
-		$("#todo_list").append('<li id="'+i+'"><a href="#">'+todo+'</a><a href="#" data-rel="dialog" data-transition="slideup" id="remove">Remove</a></li>');
-	}	
-	// Refresh list so jquery mobile can apply iphone look to the list
-	$("#todo_list").listview();
-	$("#todo_list").listview("refresh");	
-}
+// function create_new_list(){
+// 	for (var i = 0; i < localStorage.length; i++){
+// 		todo = localStorage.getItem('names_'+i);
+// 		$("#todo_list").append('<li id="'+i+'"><a href="#">'+todo+'</a><a href="#" data-rel="dialog" data-transition="slideup" id="remove">Remove</a></li>');
+// 	}	
+// 	// Refresh list so jquery mobile can apply iphone look to the list
+// 	$("#todo_list").listview();
+// 	$("#todo_list").listview("refresh");
+// }
 function save_todo(){
 	var todo = $("#textinput1").val();
 	if(todo.length){
 		// store item in local storage
-		localStorage['names_'+i] = todo;
-
-		// Update todo list
-		$("#todo_list").append('<li id="'+i+'"><a href="#">'+todo+'</a><a href="#" data-rel="dialog" data-transition="slideup" id="remove">Remove</a></li>');
+		//localStorage['names_'+i] = todo;
 		
-		// Refresh list so jquery mobile can apply iphone look to the list
-		$("#todo_list").listview();
-		$("#todo_list").listview("refresh");	
-		i++;
+		// store item in file via ajax
+		$.ajax({
+            type: 'POST',
+            url: 'ajax.php?do=save_entry&list_id=' + list_id + '&entry=' + todo,
+            contentType: "application/json; charset=utf-8",
+            data: '',
+            timeout: 500,
+            success: 'get_list'
+        })
 	}
+}
+function get_list() {
+	$.ajax({
+        type: 'POST',
+        url: 'ajax.php?do=get_list&list_id=' + list_id,
+        contentType: "application/json; charset=utf-8",
+        timeout: 500,
+        success: 'refresh_list'
+    })
+	
+	
+}
+function refresh_list($data) {
+	var data_obj = JSON.parse(data);
+	for (var a=0, a<data_obj.length, a++) {
+		$("#todo_list").append('<li id="'+a+'"><a href="#">'+data_obj[a].entry+'</a><a href="#" data-rel="dialog" data-transition="slideup" id="remove">Remove</a></li>');
+	}
+	
+	// Refresh list so jquery mobile can apply iphone look to the list
+	$("#todo_list").listview();
+	$("#todo_list").listview("refresh");	
+	i++;
 }
 </script>
