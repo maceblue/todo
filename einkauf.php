63335645499c17a1693961da754bf805c323421f
m.mende <m.mende@axilaris.de>
2017-11-29 09:58:25 +0100

    onload

--- einkauf.php
+++ einkauf.php
@@ -17,7 +17,7 @@
 	<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js">
 	</script>
 	</head> 
-	<body onload="onBodyLoad()"> 
+	<body> 
 		<div data-role="header" data-position="inline" data-theme="b">
 			<h1>Einkauf</h1>
 			<a href="add_shopping.html" data-icon="add" iconpos="notext" data-position="inline" data-rel="dialog">neuer Artikel</a>
@@ -36,7 +36,9 @@
 
 <script language="javascript">
 var list_id = 'shopping_list_1';
-
+$(document).bind("pagechange", function (event, data) {
+    onBodyLoad();
+});
 function onBodyLoad(){
 	var todo = "";
 	setTimeout(function() { window.scrollTo(0, 1) }, 100);
